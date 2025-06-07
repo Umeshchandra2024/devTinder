cb1fb6097ccac2d6fbf5f537432add4676eec5b8
umesh <umeshchandrareddy851@gmail.com>
2025-06-07 11:45:27 +0530

    Exploring get,post,delete

--- src/app.js
+++ src/app.js
@@ -5,17 +5,32 @@ const app = express();
 //     res.send("Hello from devTinder server");
 // });
 
+// app.use("/user", (req,res) => {
+//     res.send("HAHAHAHAHA");
+// });
 
-app.use("/hello", (req,res) => {
-    res.send("hello,hello from Tinder");
+app.get("/user", (req,res) => {
+    res.send({firstName: "Umesh", lastName: "Chandra"});
 });
 
-app.use("/test", (req,res) => {
-    res.send("Hello from test");
+app.post("/user", (req,res) => {
+    res.send("Data successfully to the dashboard");
 });
-app.use("/", (req,res) => {
-    res.send("hello from dashboard");
-}); 
+
+app.delete("/user", (req,res) => {
+    res.send("Deleted Successfully");
+});
+
+// app.use("/hello", (req,res) => {
+//     res.send("hello,hello from Tinder");
+// });
+
+// app.use("/test", (req,res) => {
+//     res.send("Hello from test");
+// });
+// app.use("/", (req,res) => {
+//     res.send("hello from dashboard");
+// }); 
 app.listen(7777, () => {
     console.log("server is running on port 7777....");
 });
